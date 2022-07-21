ddc04fa42dfdc7aaade5651865954f69610f6754
Natalia Homusko <avgust_net85@mail.ru>
2022-07-21 12:30:48 +0300

    ready lesson

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const getTitle = document.querySelector('.modal__title');
+console.log(getTitle);
+
+const getForm = document.querySelector('.modal__form');
+console.log(getForm);
+
+const getCheckbox = document.getElementById('discount')
+console.log(getCheckbox);
+
+const nextElemcheckbox = getCheckbox.nextElementSibling
+console.log(nextElem);
