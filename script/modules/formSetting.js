d072cb8fb1ddc03ea2ee61ad2b4a445d88ff3b1a
Natalia Homusko <avgust_net85@mail.ru>
2022-10-13 16:46:39 +0300

    base64

--- script/modules/formSetting.js
+++ script/modules/formSetting.js
@@ -25,4 +25,57 @@ export const formSetting = (form) => {
     form.count.addEventListener('blur', e => {
         e.target.form.total.value = `$${form.count.value * form.price.value}`;
     });
+
+
+    const fieldset = document.querySelector('.modal__fieldset');
+    const createPreview = () => {
+        const image = document.createElement('div');
+        image.className = 'image-container';
+        image.insertAdjacentHTML('beforeend', `<img class="preview">`);
+        fieldset.append(image);
+    };
+
+    const warnText = document.createElement('div');
+    warnText.style.cssText = `
+    color: red;
+    font-size: 14px;
+    font-weight: 600;
+    text-align: center;
+    display: none;
+    `;
+    warnText.textContent = "ИЗОБРАЖЕНИЕ НЕ ДОЛЖНО ПРЕВЫШАТЬ 1 МБ"
+
+    const toBase64 = file => new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.addEventListener('loadend', () => {
+            resolve(reader.result);
+        });
+
+        reader.addEventListener('error', err => {
+            reject(err);
+        });
+
+        reader.readAsDataURL(file);
+    });
+
+    const file = document.querySelector('.modal__file');
+    file.addEventListener('change', async () => {
+        console.log(file.files[0].size);
+        if (file.files.length > 0 && file.files[0].size <= 1000) {
+            warnText.style.display = 'none';
+            const src = URL.createObjectURL(file.files[0]);
+            createPreview();
+            const preview = document.querySelector('.preview');
+            preview.parentElement.style.display = 'block';
+            preview.src = src;
+        }
+        else if (file.files.length > 0 && file.files[0].size > 1000) {
+            if (fieldset.contains(document.querySelector('.image-container'))) {
+                document.querySelector('.image-container').style.display = 'none';
+            }
+            warnText.style.display = 'block';
+            file.before(warnText);
+        }
+    });
+
 };
